80b22fdb6ddfff9324ad4cb708b60e2f48e940ee
nannymcphee <victornguyen0705@gmail.com>
2018-04-02 17:06:52 +0700

    Done BeforeSignHomeViewController (TableView), still have auto layout issues and cell spacing issues

--- MotelNetwork/ViewController/BeforeSignHomeViewController/BeforeSignHomeViewController.swift
+++ MotelNetwork/ViewController/BeforeSignHomeViewController/BeforeSignHomeViewController.swift
@@ -8,10 +8,21 @@
 
 import UIKit
 
-class BeforeSignHomeViewController: UIViewController {
+class BeforeSignHomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+
+    @IBOutlet weak var tbListNews: UITableView!
+    @IBOutlet weak var sbSearch: UITextField!
+    @IBOutlet weak var btnNavigateLogin: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tbListNews.delegate = self
+        tbListNews.dataSource = self
+        tbListNews.register(UINib(nibName: "ListNewsTableViewCell", bundle: nil), forCellReuseIdentifier: "ListNewsTableViewCell")
 
         // Do any additional setup after loading the view.
     }
@@ -21,7 +32,54 @@ class BeforeSignHomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tbListNews.dequeueReusableCell(withIdentifier: "ListNewsTableViewCell") as! ListNewsTableViewCell
+        
+        switch indexPath.row {
+        case 0:
+            cell.lblTitle.text = "Cho thuê phòng trọ tại Thích Quảng Đức"
+            cell.lblArea.text = "30m2"
+            cell.lblPrice.text = "4.000.000đ"
+            cell.lblLocation.text = "Phú Nhuận"
+        case 1:
+            cell.lblTitle.text = "Phòng trọ cao cấp tại D2"
+            cell.lblArea.text = "40m2"
+            cell.lblPrice.text = "6.000.000đ"
+            cell.lblLocation.text = "Bình Thạnh"
+        case 2:
+            cell.lblTitle.text = "Phòng trọ mới xây quận Tân Phú"
+            cell.lblArea.text = "25m2"
+            cell.lblPrice.text = "2.500.000đ"
+            cell.lblLocation.text = "Tân Phú"
+        case 3:
+            cell.lblTitle.text = "Phòng trọ cao cấp gần chợ Hoàng Hoa Thám"
+            cell.lblArea.text = "35m2"
+            cell.lblPrice.text = "5.000.000đ"
+            cell.lblLocation.text = "Tân Bình"
+        default:
+            break
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 136
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    @IBAction func btnNavigateLoginPressed(_ sender: Any) {
+        let vc = LoginViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ MotelNetwork/ViewController/BeforeSignHomeViewController/ListNewsTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  ListNewsTableViewCell.swift
+//  MotelNetwork
+//
+//  Created by Nguyên Duy on 4/2/18.
+//  Copyright © 2018 Nguyên Duy. All rights reserved.
+//
+
+import UIKit
+
+class ListNewsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblPrice: UILabel!
+    @IBOutlet weak var lblArea: UILabel!
+    @IBOutlet weak var lblLocation: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
