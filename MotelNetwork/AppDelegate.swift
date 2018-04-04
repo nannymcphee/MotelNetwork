fe98da1bfb118f7eaa2eed308ee6cb6f50dd03ad
nannymcphee <victornguyen0705@gmail.com>
2018-04-04 16:19:42 +0700

    Renamed BeforeSignDetailRoomViewController to BeforeSignDetailNewsController

--- MotelNetwork/AppDelegate.swift
+++ MotelNetwork/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         
         
-        rootViewControler = BeforeSignDetailRoomViewController()
+        rootViewControler = BeforeSignDetailNewsViewController()
         navigationController = UINavigationController(rootViewController: rootViewControler!)
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = navigationController
