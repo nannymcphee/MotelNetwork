193a351e481f93f8abd0034f226cd2474f345b91
Phùng Trọng Huy <phungtronghuy96@gmail.com>
2018-04-07 23:11:57 -0700

    Done TutorialViewController

--- MotelNetwork/AppDelegate.swift
+++ MotelNetwork/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         FirebaseApp.configure()
         
-        rootViewControler = CreateRoomViewController()
+        rootViewControler = TutorialViewController()
         navigationController = UINavigationController(rootViewController: rootViewControler!)
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = navigationController
--- MotelNetwork/ViewController/TutorialViewController/TutorialViewController.swift
+++ MotelNetwork/ViewController/TutorialViewController/TutorialViewController.swift
@@ -10,45 +10,61 @@ import UIKit
 
 class TutorialViewController: UIViewController, UIScrollViewDelegate {
 
-    @IBOutlet weak var btnGetStarted: UIButton!
-    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var sclTutorial: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
-    
-    
-    var imageArr: [String] = ["Color Burst 1", "Color Burst 2", "Color Burst 3"]
-    var frame = CGRect(x:0, y:0, width:0, height:0)
-    
+    //    @IBOutlet weak var btnGetStarted: UIButton!
+   
+//    @IBOutlet weak var pageControl: UIPageControl!
+    var screenWidth = UIScreen.main.bounds.width
+//    
+//    
+//    var imageArr: [String] = ["Color Burst 1", "Color Burst 2", "Color Burst 3"]
+////    var frame = CGRect(x:0, y:0, width:0, height:0)
+////
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        pageControl.numberOfPages = imageArr.count
+//
+//    }
+//        for index in 0 ..< imageArr.count {
+//            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+//            frame.size = scrollView.frame.size
+//            
+//            let imgView = UIImageView(frame: frame)
+//            imgView.image = UIImage(named: imageArr[index])
+//            self.scrollView.addSubview(imgView)
+//        }
+//        
+//        scrollView.contentSize = CGSize(width: (scrollView.frame.size.width * CGFloat(imageArr.count)), height: scrollView.frame.size.height)
+//        scrollView.delegate = self
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        pageControl.numberOfPages = imageArr.count
-        for index in 0 ..< imageArr.count {
-            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
-            frame.size = scrollView.frame.size
-            
-            let imgView = UIImageView(frame: frame)
-            imgView.image = UIImage(named: imageArr[index])
-            self.scrollView.addSubview(imgView)
-        }
+        sclTutorial.delegate = self
         
-        scrollView.contentSize = CGSize(width: (scrollView.frame.size.width * CGFloat(imageArr.count)), height: scrollView.frame.size.height)
-        scrollView.delegate = self
+        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+//    
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
-        pageControl.currentPage = Int(pageNumber)
+        let contentOffsetX = sclTutorial.contentOffset.x
+        let currentPage = contentOffsetX/screenWidth
+        pageControl.currentPage = Int(currentPage)
     }
-    
     @IBAction func btnGetStartedClick(_ sender: Any) {
         let vc = LoginViewController()
         self.navigationController?.pushViewController(vc, animated: true)
     }
+    //
+//    @IBAction func btnGetStartedClick(_ sender: Any) {
+//        let vc = LoginViewController()
+//        self.navigationController?.pushViewController(vc, animated: true)
+//    }
     
     /*
     // MARK: - Navigation
