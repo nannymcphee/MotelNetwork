9f3c864460de56dbc9a5d2346d81d48b5c0c19dc
Nguyên Duy <victornguyen0705@gmail.com>
2018-04-08 21:58:58 +0700

    Revert "Done TutorialViewController"
    This reverts commit 193a351e481f93f8abd0034f226cd2474f345b91.

--- MotelNetwork/ViewController/TutorialViewController/TutorialViewController.swift
+++ MotelNetwork/ViewController/TutorialViewController/TutorialViewController.swift
@@ -10,61 +10,45 @@ import UIKit
 
 class TutorialViewController: UIViewController, UIScrollViewDelegate {
 
-    @IBOutlet weak var sclTutorial: UIScrollView!
+    @IBOutlet weak var btnGetStarted: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
-    //    @IBOutlet weak var btnGetStarted: UIButton!
-   
-//    @IBOutlet weak var pageControl: UIPageControl!
-    var screenWidth = UIScreen.main.bounds.width
-//    
-//    
-//    var imageArr: [String] = ["Color Burst 1", "Color Burst 2", "Color Burst 3"]
-////    var frame = CGRect(x:0, y:0, width:0, height:0)
-////
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        pageControl.numberOfPages = imageArr.count
-//
-//    }
-//        for index in 0 ..< imageArr.count {
-//            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
-//            frame.size = scrollView.frame.size
-//            
-//            let imgView = UIImageView(frame: frame)
-//            imgView.image = UIImage(named: imageArr[index])
-//            self.scrollView.addSubview(imgView)
-//        }
-//        
-//        scrollView.contentSize = CGSize(width: (scrollView.frame.size.width * CGFloat(imageArr.count)), height: scrollView.frame.size.height)
-//        scrollView.delegate = self
-//    }
-//
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
+    
+    
+    var imageArr: [String] = ["Color Burst 1", "Color Burst 2", "Color Burst 3"]
+    var frame = CGRect(x:0, y:0, width:0, height:0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        sclTutorial.delegate = self
+
+        pageControl.numberOfPages = imageArr.count
+        for index in 0 ..< imageArr.count {
+            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+            frame.size = scrollView.frame.size
+            
+            let imgView = UIImageView(frame: frame)
+            imgView.image = UIImage(named: imageArr[index])
+            self.scrollView.addSubview(imgView)
+        }
         
-        // Do any additional setup after loading the view.
+        scrollView.contentSize = CGSize(width: (scrollView.frame.size.width * CGFloat(imageArr.count)), height: scrollView.frame.size.height)
+        scrollView.delegate = self
     }
-//    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        let contentOffsetX = sclTutorial.contentOffset.x
-        let currentPage = contentOffsetX/screenWidth
-        pageControl.currentPage = Int(currentPage)
+        let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageControl.currentPage = Int(pageNumber)
     }
+    
     @IBAction func btnGetStartedClick(_ sender: Any) {
         let vc = LoginViewController()
         self.navigationController?.pushViewController(vc, animated: true)
     }
-    //
-//    @IBAction func btnGetStartedClick(_ sender: Any) {
-//        let vc = LoginViewController()
-//        self.navigationController?.pushViewController(vc, animated: true)
-//    }
     
     /*
     // MARK: - Navigation
